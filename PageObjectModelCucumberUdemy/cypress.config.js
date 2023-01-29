9a24d67c8c260d8432d679ae0d3722ad60bc79b5
AmbarishDash15 <ambarish.amrit@gmail.com>
2023-01-29 20:45:51 +1100

    first feature

--- PageObjectModelCucumberUdemy/cypress.config.js
+++ PageObjectModelCucumberUdemy/cypress.config.js
@@ -3,6 +3,18 @@ const {downloadFile} = require('cypress-downloadfile/lib/addPlugin');
 const marge = require('mochawesome-report-generator');
 const { merge } = require('mochawesome-merge');
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on("file:preprocessor", browserify.default(config));
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 
 
 module.exports = defineConfig({
@@ -10,6 +22,9 @@ module.exports = defineConfig({
   e2e: {
     baseUrl : 'https://www.way2automation.com/angularjs-protractor/banking/#/login',
     pageLoadTimeout : 120000,
+    specPattern: "**/*.feature",
+    setupNodeEvents,
+    /*
     setupNodeEvents(on, config) {
       // implement node event listeners here
       //this.chromeWebSecurity = false
---
+++ PageObjectModelCucumberUdemy/cypress/e2e/BankManagerLogin.feature
@@ -0,0 +1,8 @@
+Feature: BankManagerLogin
+
+    I want to login as Bank Manager
+
+    Scenario: Login as Bank Manager
+        Given I navigate to the website
+        When I click on Bank Manager Login Button
+        Then Bank Manager home page should Open
